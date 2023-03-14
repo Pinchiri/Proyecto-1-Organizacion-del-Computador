8885ac0ee02fcc0057a28ec62637f75cd46ee0c5
luism <luismanzur91@outlook.com>
2023-03-13 23:11:16 -0400

    feat: add subtraction algorithm

--- Proyecto 1, Rolando Sorrentino, Luis Manzur, David Mizrahi.asm
+++ Proyecto 1, Rolando Sorrentino, Luis Manzur, David Mizrahi.asm
@@ -13,11 +13,11 @@ msgInteger1: .asciiz "Enter the first large integer (50 characters): \n--> "
 msgInteger2: .asciiz "Enter the second large integer(50 characters): \n--> "
 msgOperation: .asciiz "Enter the number of the operation that you want to do: \n1. Addition \n2. Substraction \n3. Multiplication \n4. Exit the program \n--> "
 
-# Salto de línea
+# Salto de lï¿½nea
 salto: .asciiz "\n"
 
 # Mensajes validaciones
-msgGreaterLessThan: .asciiz "\n¡WARNING! You have to enter a number between 1 and 4 ¡WARNING!\n"
+msgGreaterLessThan: .asciiz "\nï¿½WARNING! You have to enter a number between 1 and 4 ï¿½WARNING!\n"
 
 # Mensaje Output
 msgOutput: "El resultado es: "
@@ -39,7 +39,7 @@ inputInteger:
 	li $a1, 51
 	syscall
 	
-	# Salto de línea
+	# Salto de lï¿½nea
 	li $v0, 4
 	la $a0, salto
 	syscall
@@ -55,12 +55,12 @@ inputInteger:
 	li $a1, 51
 	syscall
 	
-	# Salto de línea
+	# Salto de lï¿½nea
 	li $v0, 4
 	la $a0, salto
 	syscall
 	
-	# Últimas posiciones de los enteros
+	# ï¿½ltimas posiciones de los enteros
 	li $t8, 0
 	li $t9, 0
 
@@ -68,12 +68,12 @@ inputInteger:
 		# Cargar el Caracter al cual apunta $t8
 		lb $t1, integer1($t8)
 		
-		# Condiciones de Parada (Salto de línea = 10 en ASCII; Espacio = 32 en ASCII)
+		# Condiciones de Parada (Salto de lï¿½nea = 10 en ASCII; Espacio = 32 en ASCII)
 		beq $t1, 10, endLastPosition1
 		beq $t1, 32, endLastPosition1
 		beqz $t1, endLastPosition1
 	
-		# Iterar hasta conseguir la mayor posición del entero
+		# Iterar hasta conseguir la mayor posiciï¿½n del entero
 		addi $t8, $t8, 1
 		
 		b lastPosition1
@@ -83,22 +83,22 @@ inputInteger:
 		# Cargar el Caracter al cual apunta $t9
 		lb $t2, integer2($t9)
 		
-		# Condiciones de Parada (Salto de línea = 10 en ASCII; Espacio = 32 en ASCII)
+		# Condiciones de Parada (Salto de lï¿½nea = 10 en ASCII; Espacio = 32 en ASCII)
 		beq $t2, 10, endLastPosition2
 		beq $t2, 32, endLastPosition2
 		beqz $t2, endLastPosition2
 	
-		# Iterar hasta conseguir la mayor posición del entero
+		# Iterar hasta conseguir la mayor posiciï¿½n del entero
 		addi $t9, $t9, 1
 		
 		b lastPosition2	
 	endLastPosition2:
 	
-	# Variables de iteración
+	# Variables de iteraciï¿½n
 	li $t0, 0
 	li $t1, 0
 	
-	# Se guardan los tamaños de los dos números para usarlos posteriormente
+	# Se guardan los tamaï¿½os de los dos nï¿½meros para usarlos posteriormente
 	la $s4, ($t8)
 	la $s5, ($t9)
 	
@@ -121,7 +121,7 @@ inputInteger:
 	
 	# Invertir primer entero
 	invertion1:
-		# Condición de parada
+		# Condiciï¿½n de parada
 		blt $t8, 0, endInvertion1
 		
 		lb $t1, integer1($t8)
@@ -133,12 +133,12 @@ inputInteger:
 		b invertion1
 	endInvertion1:
 	
-	# Variable de iteración
+	# Variable de iteraciï¿½n
 	li $t0, 0
 	
 	# Invertir segundo entero
 	invertion2:
-		# Condición de parada
+		# Condiciï¿½n de parada
 		blt $t9, 0, endInvertion2
 		
 		lb $t2, integer2($t9)
@@ -154,12 +154,12 @@ inputOperation:
 	
 	li $t1, 0
 	
-	#Imprime el mensaje para introducir la operación a realizar
+	#Imprime el mensaje para introducir la operaciï¿½n a realizar
 	li $v0, 4
 	la $a0, msgOperation
 	syscall
 	
-	#Input de la operación a realizar
+	#Input de la operaciï¿½n a realizar
 	li $v0, 5
 	syscall
 	move $t1, $v0
@@ -179,7 +179,7 @@ validations:
 	
 	beq $t1, 4, end
 	
-	#Validación si inputOperation es menor que 1 o mayor que 3
+	#Validaciï¿½n si inputOperation es menor que 1 o mayor que 3
 	bgt $t1, 4, greaterThan	
 	blt $t1, 1, lessThan
 	
@@ -202,7 +202,7 @@ lessThan:
 	
 #Addition
 addition:
-	#Inicializamos la variable de iteración $t0 en 1 ya que el primer elemento (0) del número invertido es null
+	#Inicializamos la variable de iteraciï¿½n $t0 en 1 ya que el primer elemento (0) del nï¿½mero invertido es null
 	li $t0, 1
 	li $s0, 0
 	li $t9, 0
@@ -212,7 +212,7 @@ addition:
 		lb $t1, inverted1($t0)
 		lb $t2, inverted2($t0)
 		
-		# Condición de Parada (Si $t8 o $t9 son menores que 0)
+		# Condiciï¿½n de Parada (Si $t8 o $t9 son menores que 0)
 		bgt $t0, $s6, lastDigit
 		#bgt $t0, $s6, printResult
 		
@@ -229,7 +229,7 @@ addition:
 		blt $t1, 48, null1
 		blt $t2, 48, null2
 
-		# Se realiza la suma de los dos dígitos de cada entero
+		# Se realiza la suma de los dos dï¿½gitos de cada entero
 		add $t3, $t1, $t2
 		blt $t3, 58, null
 		# Se resta menos 48 para dar el valor correcto de la suma en ASCII
@@ -247,15 +247,15 @@ addition:
 		
 			
 		null:
-		# Si la suma de los dos dígitos es mayor que 9 (57 en ASCII) entramos en "greaterTen"
+		# Si la suma de los dos dï¿½gitos es mayor que 9 (57 en ASCII) entramos en "greaterTen"
 		bgt $t4, 57, greaterTen
-		# Si la suma de los dos dígitos es menor que 10 (58 en ASCII) entramos en "lessTen"
+		# Si la suma de los dos dï¿½gitos es menor que 10 (58 en ASCII) entramos en "lessTen"
 		ble $t4, 57, lessTen
 		
 		greaterTen:
 			beqz $t8, endChangeCarry
 			changeCarry:
-				# Se suma el Cociente acarreado en la anterior iteración y se vuelve a declarar $t8 como 0
+				# Se suma el Cociente acarreado en la anterior iteraciï¿½n y se vuelve a declarar $t8 como 0
 				li $t7, 0
 				add $t7, $t7, $t8
 				li $t8, 0
@@ -263,16 +263,16 @@ addition:
 			
 			# Se resta menos 48 para conseguir el valor real de la suma
 			subi $t5, $t4, 48
-			# Se divide entre 10 para obtener el Resto (Dígito que va dentro del resultado) y Cociente (Dígito que acarreamos)
+			# Se divide entre 10 para obtener el Resto (Dï¿½gito que va dentro del resultado) y Cociente (Dï¿½gito que acarreamos)
 			div $t6, $t5, 10
 			# Se mueve el Cociente a $t8 y el Resto a $t9
 			mflo $t8
 			mfhi $t9
 			
-			# Sumamos el Cociente acarreado de la anterior iteración (Si es 0 no afecta)
+			# Sumamos el Cociente acarreado de la anterior iteraciï¿½n (Si es 0 no afecta)
 			add $t9, $t9, $t7
 			
-			# Se suma al Resto de la división 48 (Ya que es el número que guardaremos en el vector resultado"
+			# Se suma al Resto de la divisiï¿½n 48 (Ya que es el nï¿½mero que guardaremos en el vector resultado"
 			addi $t9, $t9, 48
 			
 			
@@ -283,15 +283,15 @@ addition:
 		endGreaterTen:
 		
 		lessTen:
-			# Si el Cociente acarreado en la anterior iteración es 0 saltamos a "endCarry2"
+			# Si el Cociente acarreado en la anterior iteraciï¿½n es 0 saltamos a "endCarry2"
 			beqz $t8, endCarry2
 			carry2:
-				# Se suma el Cociente acarreado en la anterior iteración y se vuelve a declarar $t8 como 0
+				# Se suma el Cociente acarreado en la anterior iteraciï¿½n y se vuelve a declarar $t8 como 0
 				add $t4, $t4, $t8
 				li $t8, 0
 			endCarry2:
 			
-			# Se guarda el valor en ASCII de la suma (Este es el caso en que la suma de ambos dígitos es menor a 10)
+			# Se guarda el valor en ASCII de la suma (Este es el caso en que la suma de ambos dï¿½gitos es menor a 10)
 			sb $t4, resultInverted($s0)
 		endLessTen:
 	
@@ -304,8 +304,70 @@ addition:
 
 #Sustraction
 sustraction:
+	#Inicializamos la variable de iteraciï¿½n $t0 en 1 ya que el primer elemento (0) del nï¿½mero invertido es null
+	li $t0, 1
+	li $s0, 0
+	li $t8, 0 # Carreo
+	li $t3, 0 # result
 
-	j end
+	subt:
+	# Cargar el Caracter al cual apunta $t0
+		lb $t1, inverted1($t0)
+		lb $t2, inverted2($t0)
+
+	afterRemoveCarry:
+		# Condiciï¿½n de Parada
+		bgt $t0, $s6, printResult
+
+		beq $t8, 1, removeCarry # si hay carreo se le resta 1 $t1
+
+		#Chequea que los numeros sean de igual longitud
+		blt $t1, 48, subtNull1
+		blt $t2, 48, subtNull2
+
+		bgt $t2, $t1, addCarry
+
+		j applySubt
+
+		subtNull1:
+			la $t3, ($t2)
+			sb $t3, resultInverted($s0)
+			j subtEndCarry
+		
+		subtNull2:
+			la $t3, ($t1)
+			sb $t3, resultInverted($s0)
+			j subtEndCarry
+
+		applySubt:
+			# restamos $t1 - $t2
+			sub $t3, $t1, $t2
+			addi $t3, $t3, 48
+
+			sb $t3, resultInverted($s0)
+
+			j subtEndCarry
+			
+
+		removeCarry:
+			subi $t1, $t1, 1
+			li $t8, 0
+
+			j afterRemoveCarry
+		
+		addCarry:
+			addi $t1, $t1, 10
+			li $t8, 1
+			j applySubt
+
+
+		
+		subtEndCarry:
+			# Incrementamos $t0 = $t0 + 1; $s0 = $s0 + 1
+			addi $t0, $t0, 1 
+			addi $s0, $s0, 1
+
+		b subt
 	
 #Multiplication
 multiplication:
@@ -320,12 +382,12 @@ printResult:
 		# Cargar el Caracter al cual apunta $t0
 		lb $t1, resultInverted($t0)
 		
-		# Condiciones de Parada (Salto de línea = 10 en ASCII; Espacio = 32 en ASCII)
+		# Condiciones de Parada (Salto de lï¿½nea = 10 en ASCII; Espacio = 32 en ASCII)
 		beq $t1, 10, endLastPositionResult
 		beq $t1, 32, endLastPositionResult
 		beqz $t1, endLastPositionResult
 	
-		#Iterar hasta conseguir la mayor posición del resultado
+		#Iterar hasta conseguir la mayor posiciï¿½n del resultado
 		addi $t0, $t0, 1
 		
 		b lastPositionResult
@@ -335,9 +397,9 @@ printResult:
 	li $t1, 0
 	subi $t0, $t0, 1
 	
-	#Invertimos el resultado para devolverlo a su posición correcta
+	#Invertimos el resultado para devolverlo a su posiciï¿½n correcta
 	resultInvertion:
-		# Condición de parada
+		# Condiciï¿½n de parada
 		blt $t0, 0, endResultInvertion
 		
 		lb $t1, resultInverted($t0)
@@ -351,7 +413,7 @@ printResult:
 	endResultInvertion:
 	
 	
-	# Salto de línea
+	# Salto de lï¿½nea
 	li $v0, 4
 	la $a0, salto
 	syscall
